5788a1e3e0577748b3ff2023f17b2c16e707d29c
Camila Onofri <onofricamila2@gmail.com>
2019-08-30 11:20:26 -0300

    bases laptop battery level under 35%

--- batteryfull.sh
+++ batteryfull.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
+
+# requires one arg representing the message to show
+notify_something () {
+	notify-send -u critical -t 0 -i "$PWD/batteryfull.png" "$1"
+	paplay /usr/share/sounds/ubuntu/stereo/message.ogg
+}
+
+# main loop
 while true
 do
     export DISPLAY=:0.0
     battery_percent=$(acpi -b | grep -P -o '[0-9]+(?=%)')
     if on_ac_power; then
-        if [ "$battery_percent" -gt 95 ]; then
-            notify-send -i "$PWD/batteryfull.png" "Battery full." "Level: ${battery_percent}% "
-            paplay /usr/share/sounds/ubuntu/ringtones/Alarm\ clock.ogg
+        if [ "$battery_percent" ">" "80" ]; then
+		notify_something "Laptop battery is almost fully charged. Unplug charger"
         fi
+    elif [ "$battery_percent" "<" "35" ]; then
+	    notify_something "Plug laptop charger. Battery level under 35%."
     fi
     sleep 300 # (5 minutes)
 done
